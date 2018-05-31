6ebef2ff7808b328f7eefc7ece1b73aa2626ca0a
Ritter Gustave <Gustaver@uci.edu>
2018-05-31 14:10:28 -0700

    Getting started

---
+++ app.py
@@ -0,0 +1,107 @@
+from flask import Flask, jsonify, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    """returns dashboard homepage"""
+    return render_template('index.html')
+
+
+@app.route("/names")
+def names():
+    """
+    List of Simple Names
+    Return a simple list of Names
+       [
+        "BB_940",
+        "BB_941",
+        "BB_943",
+        "BB_944",
+        "BB_945",
+        "BB_946",
+        "BB_947",
+        ...
+    ]
+    """
+    return render_template('index.html')
+
+@app.route('/otu')
+def otu():
+    """List of OTU descriptions.
+
+    Returns a list of OTU descriptions in the following format
+
+    [
+        "Archaea;Euryarchaeota;Halobacteria;Halobacteriales;Halobacteriaceae;Halococcus",
+        "Archaea;Euryarchaeota;Halobacteria;Halobacteriales;Halobacteriaceae;Halococcus",
+        "Bacteria",
+        "Bacteria",
+        "Bacteria",
+        ...
+    ]
+    """
+    return render_template('index.html')
+
+@app.route('/metadata/<sample>')
+def metadata():
+    """MetaData for a given sample.
+
+    Args: Sample in the format: `BB_940`
+
+    Returns a json dictionary of sample metadata in the format
+
+    {
+        AGE: 24,
+        BBTYPE: "I",
+        ETHNICITY: "Caucasian",
+        GENDER: "F",
+        LOCATION: "Beaufort/NC",
+        SAMPLEID: 940
+    }
+    """
+    return render_template('index.html')
+
+
+@app.route('/wfreq/<sample>')
+def wfreq():
+    """Weekly Washing Frequency as a number.
+
+    Args: Sample in the format: `BB_940`
+
+    Returns an integer value for the weekly washing frequency `WFREQ`
+    """
+    return render_template('index.html')
+
+
+@app.route('/samples/<sample>')
+def samples():
+    """OTU IDs and Sample Values for a given sample.
+
+    Sort your Pandas DataFrame (OTU ID and Sample Value)
+    in Descending Order by Sample Value
+
+    Return a list of dictionaries containing sorted lists  for `otu_ids`
+    and `sample_values`
+
+    [
+        {
+            otu_ids: [
+                1166,
+                2858,
+                481,
+                ...
+            ],
+            sample_values: [
+                163,
+                126,
+                113,
+                ...
+            ]
+        }
+    ]
+    """
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
