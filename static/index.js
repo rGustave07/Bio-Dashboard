854fb10bd859187a96987d0f50b0bfe9e1c8133c
Ritter Gustave <rGustave5@gmail.com>
2018-06-05 01:41:24 -0700

    backend link for names functional, option change functional

--- static/index.js
+++ static/index.js
@@ -1,6 +1,25 @@
 window.onload = (e) => {
-  console.log("document ready:", e);
+  populateSampleNames();
 
-  document.getElementById('inputGroupSelect01').
+}
 
+let populateSampleNames = () => {
+  let array;
+  Plotly.d3.json('/names', (error, data) => {
+    if(error){
+      console.log(error);
+    } else {
+        data.forEach( element => {
+            let option = document.createElement("option");
+            let t = document.createTextNode(element);
+            option.appendChild(t);
+            document.getElementById("inputGroupSelect01").appendChild(option);
+        });
+    }
+  });
+}
+
+let onSampleChange = (e) => {
+  console.log("Changed to: ", e.value);
+  return e.value
 }
